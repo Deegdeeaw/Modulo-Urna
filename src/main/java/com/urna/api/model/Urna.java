c7e35afda50d1cf1a5a03d0a95760c90a41693b9
Iuri Cunha <yurycunhah@gmail.com>
2026-03-31 21:34:00 -0300

    Iniciando Projeto

---
+++ src/main/java/com/urna/api/model/Urna.java
@@ -0,0 +1,15 @@
+package com.urna.api.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = urnas)
+
+public class Urna {
+    @Id
+
+
+}
